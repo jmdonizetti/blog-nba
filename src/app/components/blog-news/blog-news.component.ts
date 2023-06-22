d36b00ebdbaeb836caa258c63b164244c26743e7
Joao <j_donizetti@outlook.com>
2023-06-22 20:57:53 -0300

    initial commit

---
+++ src/app/components/blog-news/blog-news.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-news',
+  templateUrl: './blog-news.component.html',
+  styleUrls: ['./blog-news.component.css',
+  './blog-news.responsive.component.css']
+})
+export class BlogNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,44 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"DENVER NUGGETS",
+    "description":"For the first time in their history, DENVER NUGGETS are NBA champions",
+    "photoCover":"https://logosmarcas.net/wp-content/uploads/2020/07/Denver-Nuggets-Logo.png"
+  },
+  {
+    "id":"2",
+    "title":"CHAMPIONS NBA",
+    "description":"These are the top five NBA champions right now",
+    "photoCover":"https://pbs.twimg.com/media/FVbLlqkWIAMf7Nh.jpg:large"
+  },
+  {
+    "id":"3",
+    "title":"GAME NBA 2K23",
+    "description":"NBA 2023 was nominated for Best Sports/Racing Game at the game awards, but ended up losing to Gran Turismo 7",
+    "photoCover":"https://image.api.playstation.com/vulcan/ap/rnd/202206/2202/g7tfPrQ4WnPzvI4lKq28zx9n.jpg"
+  },
+  {
+    "id":"4",
+    "title":"NIKOLA JOKIC",
+    "description":"Points: 29.9 / Rebounds: 13.3 / Assists: 10.3 / Total triple-doubles: 8 (historic record)",
+    "photoCover":"https://www.solobasket.com/sites/default/files/styles/solobasket_detail_header_big/public/nikola_jokic_mvp.jpeg?itok=R0LQeoQw"
+  },
+  {
+    "id":"5",
+    "title":"DRAFT 2023",
+    "description":"recruiting new talents for nba",
+    "photoCover":"https://medias.itatiaia.com.br/generic/jogadores-serao-escolhidos-nesta-quinta-feira-22-c02aedf2-3cc2-43b5-853d-d82bddf1866c.medium.webp"
+  },
+  {
+    "id":"6",
+    "title":"NBA's Greatest Scorers",
+    "description":"List of the greatest scorers in nba history",
+    "photoCover":"https://www.estadao.com.br/resizer/mtBj93dTV7EMYnECuhOVfF3xIus=/380x0/filters:format(png):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/estadao/4DGCOIECWNDJRNTA74G3F2P33E.png"
+  },
+  {
+    "id":"7",
+    "title":"JOÃO MARCOS D. RODRIGUES",
+    "description":"Angular front-end developer",
+    "photoCover":"https://assets-blog.hostgator.com.br/wp-content/uploads/2021/03/logo-angular.webp"
+  },
+ ]
